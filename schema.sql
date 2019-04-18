f728c4cc78164c743ef173b50cbf1ff7c96d9686
AnnaGH1 <annabekkarevich@gmail.com>
2019-04-17 21:06:19 -0400

    database schema edits

--- schema.sql
+++ schema.sql
@@ -1,68 +1,75 @@
+CREATE SCHEMA `627089-yeticave` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `627089-yeticave`;
 
 
-CREATE TABLE categories (
-  cat_id INT AUTO_INCREMENT PRIMARY KEY,
-  cat_name VARCHAR(64) NOT NULL
+CREATE TABLE category (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(64) NOT NULL,
+
+  PRIMARY KEY (id)
 );
 
 
-CREATE TABLE users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  user_name VARCHAR(64) NOT NULL,
-  user_email VARCHAR(64) NOT NULL,
-  user_password VARCHAR(64) NOT NULL,
-  user_contact VARCHAR(256) NOT NULL,
-  user_dtreg DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  user_avatar VARCHAR(64) DEFAULT NULL,
+CREATE TABLE user (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(64) NOT NULL,
+  email VARCHAR(64) NOT NULL,
+  password VARCHAR(64) NOT NULL,
+  contact VARCHAR(256) NOT NULL,
+  dt_reg DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  avatar VARCHAR(64) DEFAULT NULL,
 
-  CONSTRAINT user_unique UNIQUE (user_email)
+  PRIMARY KEY (id),
+  CONSTRAINT user_unique UNIQUE (email)
 );
 
 
-CREATE TABLE lots (
-  lot_id INT AUTO_INCREMENT PRIMARY KEY,
-  lot_name VARCHAR(64) NOT NULL,
-  lot_description VARCHAR(256) NOT NULL,
-  lot_image VARCHAR(64) NOT NULL,
-  lot_dtstart DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  lot_dtend DATETIME NOT NULL,
-  lot_price INT NOT NULL,
-  lot_increment INT NOT NULL,
+CREATE TABLE lot (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(64) NOT NULL,
+  description VARCHAR(256) NOT NULL,
+  image VARCHAR(64) NOT NULL,
+  dt_start DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dt_end DATETIME NOT NULL,
+  price INT NOT NULL,
+  increment INT NOT NULL,
 
-  cat_id INT NOT NULL,
-  CONSTRAINT cat_valid
-  FOREIGN KEY fk_cat (cat_id)
-    REFERENCES categories (cat_id),
+  category_id INT NOT NULL,
+  CONSTRAINT category_valid
+  FOREIGN KEY fk_category (category_id)
+    REFERENCES category (id),
 
   seller INT NOT NULL,
   CONSTRAINT seller_valid
   FOREIGN KEY fk_seller (seller)
-    REFERENCES users (user_id),
+    REFERENCES user (id),
 
   winner INT DEFAULT NULL,
   CONSTRAINT winner_valid
   FOREIGN KEY fk_winner (winner)
-    REFERENCES users (user_id),
+    REFERENCES user (id),
 
-  INDEX (lot_description)
+  PRIMARY KEY (id),
+  INDEX (name, description)
 );
 
 
-CREATE TABLE bids (
-  bid_amount INT NOT NULL,
-  bid_dt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE bid (
+  id INT AUTO_INCREMENT,
+  amount INT NOT NULL,
+  dt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   lot_id INT NOT NULL,
   CONSTRAINT lot_valid
   FOREIGN KEY fk_lot (lot_id)
-    REFERENCES lots (lot_id),
+    REFERENCES lot (id),
 
   buyer INT NOT NULL,
   CONSTRAINT buyer_valid
   FOREIGN KEY fk_buyer (buyer)
-    REFERENCES users (user_id),
+    REFERENCES user (id),
 
+  PRIMARY KEY (id),
   CONSTRAINT bid_limit UNIQUE (lot_id, buyer)
 );
 
